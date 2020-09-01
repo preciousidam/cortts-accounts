30286713fab2da95934969ab8f1ea4f5991be3c1
preciousidam <preciousidam@gmail.com>
2020-09-01 15:45:56 +0100

    ...

---
+++ pages/budget/[id].js
@@ -0,0 +1,17 @@
+import React, {useState} from 'react';
+
+import MainLayout from "../../layouts/mainLayout";
+import {ProtectRoute} from '../../utility/route';
+
+
+export function Id(){
+    return (
+        <MainLayout title="Expenses Account">
+            <div className="body">
+                
+            </div>
+        </MainLayout>
+    )
+}
+
+export default ProtectRoute(Id);
